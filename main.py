28b108e2ffb0ba8f1182ab639f42ba2fd7283521
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2022-02-04 09:51:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 userType = 0
-userChoice = 0
 publicChoice = 0
+privateChoice = 0
+messageChoice = 0
 publicMessage = ""
+privateMessage = ""
+signature = ""
 
 while True:
     print("RSA Keys has been generated")
@@ -11,23 +14,53 @@ while True:
     print("\n3. Exit program")
     userType = input("Enter your choice: ")
     
-    if userType == 1:
+    while userType == 1:
         print("As a public user, what would you like to do?")
         print("\n1. Send an encrypted message")
         print("\n2. Authenticate a digital signature")
         print("\n3. Exit")
-        userChoice = input("Enter your choice: ")
+        publicChoice = input("Enter your choice: ")
         
-        if userChoice == 1:
+        if publicChoice == 1:
             publicMessage = input("Enter a message: ")
             print("Message encrypted and sent")
+            
+        if publicChoice == 2:
+            if signature is None:
+                print("There are no signature to authenticate")
+            else:
+                print("The following messages are available: ")
+                print("\n1. ", signature)
         
-    if userType == 2:
+        if publicChoice == 3:
+            userType = 0
+                
+    while userType == 2:
         print("As the owner of the keys, what would you like to do?")
         print("\n1. Decrypt a received message")
         print("\n2. Digitally sign a message")
         print("\n3. Exit")
-        userChoice = input("Enter your choice: ")
-    
-    else:
+        privateChoice = input("Enter your choice: ")
+        
+        if privateChoice == 1:
+            if privateMessage is None:
+                print("There are no messages available")
+            else:
+                print("The following messages are available: ")
+                print("\n1. (length = ", len(privateMessage), ")")
+                messageChoice = input("Enter your choice: ")
+                
+                if messageChoice == 1:
+                    print("Decrypted message: ")
+                    #CODE HERE
+                
+        if privateChoice == 2:
+            privateMessage = input("Enter a message: ")
+            #CODE HERE
+            print("Message signed and sent")
+            
+        if privateChoice == 3:
+            userType = 0
+                
+    if userType == 3:
         break
