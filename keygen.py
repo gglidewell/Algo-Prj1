fc7d46681f640f3635273abacb0459bba2bc925c
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2022-02-16 15:37:45 -0600

    Revert "Revert "Merge branch 'main' of https://github.com/ManiacalManiac/Algo-Prj1""
    This reverts commit 4b67e4ec8d679003f0ddb8d104cde62ce469b221.

--- keygen.py
+++ keygen.py
@@ -1,21 +1,29 @@
 from primegen import primeGen
 import random
+import math
+
+#REF FROM https://stackoverflow.com/questions/55554166/how-to-calculate-rsa-private-key-in-python/55554571
 
 class publicKey:
     n = 0
     e = 0
     phi = 0
 
+def eGen(phi):
+    e = 3
 
-def eGen(n, phi):
-    valid = False
-    
-    #check not a factor of n
-    while not valid:
-        num = random.randint(2, phi)
-        if n % num != 0:
-            valid = True
-    return num
+    while e <= phi:
+        a = phi
+        b = e
+
+        while b:
+            a, b = b, a % b
+
+        if a == 1:
+            return e
+        else:
+            e += 2
+
 
 def pubGen():
     key = publicKey()
@@ -24,12 +32,25 @@ def pubGen():
     q = primeGen()
     key.n = p*q
     key.phi = (p-1)*(q-1)
-    key.e = eGen(key.n, key.phi)
+    key.e = eGen(key.phi)
+    
     return key
     
 
-def privGen(phi,e):
-    k = random.randint(2, 100)
-    d = (k*phi + 1)/e
-    
-    return int(d)
+
+def euclid(a, b):
+
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = euclid(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def privGen(e, phi):
+    g, x, y = euclid(e, phi)
+
+    if g != 1:
+        raise Exception('Modular inverse does not exist')
+    else:
+        return x % phi
