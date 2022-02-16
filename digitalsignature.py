e61abefddca409a55004fd56a9d9f68d8d21f1cd
ManiacalManiac <jackglide@hotmail.com>
2022-02-16 16:08:14 -0600

    Update digitalsignature.py

--- digitalsignature.py
+++ digitalsignature.py
@@ -1,11 +1,8 @@
-def SigEncrypt(sig, d, n):
-     for x in sig:
-        x = pow(ord(x),d,n)
-        auth.append(x)
+import math
 
-def SigDecrypt(e, n):
-    name = ""
-    for x in auth:
-        x = pow(x,e,n)
-        name += chr(x)
-    return(name)
+def SigEncrypt(c, d):
+    S = (c**d) % n
+
+def SigDecrypt(S, e, n):
+    I = (S**e) % n
+
